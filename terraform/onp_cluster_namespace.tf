33b1f220d9eb6618fb0d7bceb9e902be9dafbd97
kaname <kaname.imaichi@gmail.com>
2024-08-20 19:35:51 +0900

    fix path

--- terraform/onp_cluster_namespace.tf
+++ terraform/onp_cluster_namespace.tf
@@ -3,3 +3,8 @@ resource "kubernetes_namespace" "onp_cluster_argocd" {
     name = "argocd"
   }
 }
+resource "kubernetes_namespace" "onp_cluster_cloudflared-tunnel-exits" {
+  metadata {
+    name = "cloudflared-tunnel-exits"
+  }
+}
